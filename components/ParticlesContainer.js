d416be5cf4f6e5ca075d97df087881f77684684a
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-11 20:52:41 +0600

    Particles Implemented On Home Page

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -32,9 +32,62 @@ const ParticlesContainer = () => {
 						onHover: {
 							enable: true,
 							mode: "repulse",
-						}
+						},
+						resize: true,
+					},
+					modes: {
+						push: {
+							quantity: 90,
+						},
+						repalse: {
+							distance: 200,
+							duration: 0.4,
+						},
 					},
 				},
+				particles: {
+					color: {
+						value: "#E68E2E",
+					},
+					links: {
+						color: "#F5D393",
+						distance: 150,
+						enable: true,
+						opacity: 0.5,
+						width: 1,
+					},
+					collisions: {
+						enable: true,
+					},
+					move: {
+						direction: "none",
+						enable: true,
+						outMode: {
+							default: "bounce",
+						},
+						random: false,
+						speed: 1,
+						straight: false,
+					},
+					number: {
+						density: {
+							enable: true,
+							area: 800,
+						},
+						value: 80,
+					},
+					opacity: {
+						value: 0.5,
+					},
+					shape: {
+						type: "circle",
+					},
+					size: {
+						min: 1,
+						max: 5,
+					},
+				},
+				detectRetina: true,
 			}}
 		/>
 	);
