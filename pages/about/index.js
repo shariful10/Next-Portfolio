1a32dcfb02bced5d74013c31e34513b1af74cf0f
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-12 02:29:43 +0600

    Tab Data Implemented On About Page

--- pages/about/index.js
+++ pages/about/index.js
@@ -44,50 +44,50 @@ export const aboutData = [
 			},
 		],
 	},
-	{
-		title: "awards",
-		info: [
-			{
-				title: "Webby Awards - Honoree",
-				stage: "2011 - 2012",
-			},
-			{
-				title: "Adobe Design Achievement Awards - Finalist",
-				stage: "2009 - 2010",
-			},
-		],
-	},
+	// {
+	// 	title: "awards",
+	// 	info: [
+	// 		{
+	// 			title: "Webby Awards - Honoree",
+	// 			stage: "2011 - 2012",
+	// 		},
+	// 		{
+	// 			title: "Adobe Design Achievement Awards - Finalist",
+	// 			stage: "2009 - 2010",
+	// 		},
+	// 	],
+	// },
 	{
 		title: "experience",
 		info: [
 			{
 				title: "UX/UI Designer - XYZ Company",
-				stage: "2012 - 2023",
+				stage: "2022 - 2023",
 			},
 			{
-				title: "Web Developer - ABC Agency",
-				stage: "2010 - 2012",
+				title: "Web Developer - XYZ Company",
+				stage: "2022 - 2023",
 			},
-			{
-				title: "Intern - DEF Corporation",
-				stage: "2008 - 2010",
-			},
+			// {
+			// 	title: "Intern - XYZ Company",
+			// 	stage: "2022 - 2023",
+			// },
 		],
 	},
 	{
 		title: "credentials",
 		info: [
+			// {
+			// 	title: "Web Development - Programming Hero, Dhanmondi, Dhaka",
+			// 	stage: "2011",
+			// },
 			{
-				title: "Web Development - ABC University, LA, CA",
-				stage: "2011",
+				title: "Certified MERN Stack Web Developer - Programming Hero, Dhanmondi, Dhaka",
+				stage: "2023",
 			},
 			{
-				title: "Computer Science Diploma - AV Technical Institute",
-				stage: "2009",
-			},
-			{
-				title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
-				stage: "2006",
+				title: "Wordpress Developer - Soft-tech IT Institute, Uttara, Dhaka",
+				stage: "2019",
 			},
 		],
 	},
@@ -109,7 +109,7 @@ const About = () => {
 			>
 				<Avatar />
 			</motion.div>
-			<div className="container mx-auto flex flex-col items-center xl:flex-row gap-x-6">
+			<div className="container mx-auto h-screen flex flex-col items-center xl:flex-row gap-x-6">
 				<div>text</div>
 				<div>
 					<div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
@@ -126,6 +126,14 @@ const About = () => {
 									{item.title}
 								</div>
 							);
+						})}
+					</div>
+					<div>
+						{aboutData[index].info.map((item, i) => {
+							return <div key={i}>
+								{/* <====<<==== Title ====>>====> */}
+								<div>{item.title}</div>
+							</div>
 						})}
 					</div>
 				</div>
