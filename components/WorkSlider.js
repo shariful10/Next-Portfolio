88f6b127f6b0354380e0cc08278f3eb1876e0241
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-14 23:05:48 +0600

    Text & Icon Implementd On Slider Image

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -2,7 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
-import { BsArrowTopRight } from "react-icons/bs";
+import {BsArrowRight} from "react-icons/bs";
 import Image from "next/image";
 
 // <====<<==== Data ====>>====>
@@ -77,8 +77,19 @@ const WorkSlider = () => {
 													height={300}
 													alt=""
 												/>
-												{/* <====<<==== Overlay Gradient ====>>====> */}
+												{/* <====<<==== Overlay ====>>====> */}
 												<div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+												{/* <====<<==== Title ====>>====> */}
+												<div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+													<div className="flex items-center gap-x-2 text-[13px] tracking-[0.2rem]">
+														{/* Title Part 1 */}
+														<div className="delay-100">LIVE</div>
+														{/* Title Part 2 */}
+														<div className="translate-y-[500%] group-hover:translate-y-0">PROJECT</div>
+														{/* <===<<=== Icon ===>>===> */}
+														<div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200"><BsArrowRight/></div>
+													</div>
+												</div>
 											</div>
 										</div>
 									</div>
