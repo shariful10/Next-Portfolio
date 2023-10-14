713c39377368b8b68258783d1caee8f103342897
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-15 05:29:24 +0600

    Hover Effect Implement On Button

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -23,7 +23,7 @@ const Contact = () => {
 						></textarea>
 						<button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
 							<span className="group-hover:-translate-y-[120px] group-hover:opacity-0 transition-all duration-500">Let&apos;s Talk</span>
-							<BsArrowRight className="" />
+							<BsArrowRight className="translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px] text-accent" />
 						</button>
 					</form>
 				</div>
