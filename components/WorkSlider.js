5094b14c0919f97f9002d39b3c971ac3e07562a7
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-14 15:33:28 +0600

    Hover Effect Implement On Slider Image

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -59,21 +59,32 @@ const WorkSlider = () => {
 				clickable: true,
 			}}
 			modules={[Pagination]}
-			className="mySwiper h-[240px] sm:h-[340px]"
+			className="mySwiper h-[280px] sm:h-[480px]"
 		>
 			{workSlides.slides.map((slide, i) => {
 				return (
 					<SwiperSlide key={i}>
-						<div className="grid grid-cols-2 grid-rows-2 gap-4">
-              {slide.images.map((image, i) => {
-                return <div key={i}>
-                  <div>
-                  {/* // <====<<==== Image ====>>====> */}
-                  <Image src={image.path} width={500} height={300} alt=""/>
-                  </div>
-                </div>
-              })}
-            </div>
+						<div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+							{slide.images.map((image, i) => {
+								return (
+									<div key={i}>
+										<div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+											<div className="flex items-center justify-center relative overflow-hidden">
+												{/* <====<<==== Image ====>>====> */}
+												<Image
+													src={image.path}
+													width={500}
+													height={300}
+													alt=""
+												/>
+												{/* <====<<==== Overlay Gradient ====>>====> */}
+												<div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+											</div>
+										</div>
+									</div>
+								);
+							})}
+						</div>
 					</SwiperSlide>
 				);
 			})}
