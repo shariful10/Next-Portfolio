b8e17fe9b0474c76b015be9093bdfd2da28a0d00
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-15 02:07:16 +0600

    Quote Implemented On Testimonials Slider

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -56,13 +56,13 @@ const TestimonialSlider = () => {
 								</div>
 							</div>
 							{/* <==<<=== Quote & Message ===>>==> */}
-							<div>
+							<div className="bg-pink-500/10 flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
 								{/* <==<<=== Quote Icon ===>>==> */}
 								<div className="mb-4">
 									<FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
 								</div>
 								{/* <==<<=== Message ===>>==> */}
-								<div>{person.message}</div>
+								<div className="xl:text-lg text-center md:text-left">{person.message}</div>
 							</div>
 						</div>
 					</SwiperSlide>
