bddb520aea90b967c51a69630e44bf5057378a5e
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-14 23:57:28 +0600

    Slider Implemented On Testimonials Page

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,10 +1,16 @@
+import TestimonialSlider from "../../components/TestimonialSlider";
+
 const Testimonials = () => {
 	return (
 		<div className="h-screen bg-primary/30 py-32 text-center">
 			<div className="container mx-auto h-full flex flex-col justify-center">
-				<h2 className="h2">
+				<h2 className="h2 mb-8 xl:mb-0">
 					What clients <span className="text-accent">say.</span>
 				</h2>
+				{/* Slider */}
+				<div>
+					<TestimonialSlider />
+				</div>
 			</div>
 		</div>
 	);
