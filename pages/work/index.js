63b27d4bd9e1fe0d16b0d2d0795a7ea33ef14dea
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-14 14:01:49 +0600

    Some Text Implemented On Work Page

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,53 @@
+import { motion } from "framer-motion";
+import { fadeIn } from "./../../variants";
+import Bulb from "./../../components/Bulb";
+import Circles from "./../../components/Circles";
+import WorkSlider from "../../components/WorkSlider";
+
 const Work = () => {
-  return <div>Work</div>;
+	return (
+		<div className="h-screen bg-primary/30 py-36 flex items-center">
+			<Circles />
+			<div className="container mx-auto">
+				<div className="flex flex-col xl:flex-row gap-x-8">
+					{/* <====<<==== Text ====>>====> */}
+					<div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+						<motion.h2
+							variants={fadeIn("up", 0.2)}
+							initial="hidden"
+							animate="show"
+							exit="hidden"
+							className="h2 xl:mt-8"
+						>
+							My work<span className="text-accent">.</span>
+						</motion.h2>
+						<motion.p
+							variants={fadeIn("up", 0.4)}
+							initial="hidden"
+							animate="show"
+							exit="hidden"
+							className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+						>
+							Lorem ipsum dolor sit, amet consectetor adipisicing
+							elit. A eveniet dignissimos distinctio tempore
+							harmore landiuen.
+						</motion.p>
+					</div>
+					{/* <====<<==== Slider ====>>====> */}
+					<motion.div
+						variants={fadeIn("down", 0.6)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						className="w-full xl:max-w-[65%]"
+					>
+						<WorkSlider />
+					</motion.div>
+				</div>
+			</div>
+			<Bulb />
+		</div>
+	);
 };
 
 export default Work;
