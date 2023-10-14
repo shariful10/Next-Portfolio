1b9d8d2c3778ce12cde9d8a0b32bc9a3542b8647
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-14 12:32:06 +0600

    Group Hover Implemented On Card

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,28 +1,28 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-// import "swiper/css";
 import "swiper/css/pagination";
 import { FreeMode, Pagination } from "swiper/modules";
-// import { Pagination } from 'swiper/modules';
+import { AiFillHtml5 } from "react-icons/ai";
+import { SiCss3 } from "react-icons/si";
 import {
 	RxCrop,
-	RxPencil2,
-	RxDesktop,
 	RxReader,
 	RxRocket,
+	RxDesktop,
+	RxPencil2,
 	RxArrowTopRight,
 } from "react-icons/rx";
 
 // <====<<==== Data ====>>====>
 const serviceData = [
 	{
-		icon: <RxCrop />,
-		title: "Branding",
+		icon: <AiFillHtml5 />,
+		title: "HTML",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	},
 	{
-		icon: <RxPencil2 />,
-		title: "Design",
+		icon: <SiCss3 />,
+		title: "CSS",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	},
 	{
@@ -65,17 +65,17 @@ const ServiceSlider = () => {
 			{serviceData.map((item, i) => {
 				return (
 					<SwiperSlide key={i}>
-						<div className="bg-[#412f7b26] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer">
+						<div className="bg-[#412f7b26] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89, 65, 169, 0.15)] transition-all duration-300">
 							{/* <====<<==== Icon ====>>====> */}
-							<div>{item.icon}</div>
+							<div className="text-4xl text-accent mb-4">{item.icon}</div>
 							{/* <====<<==== Title & Desc ====>>====> */}
-							<div>
-								<div>{item.title}</div>
-								<p>{item.description}</p>
+							<div className="mb-8">
+								<div className="mb-2 text-lg">{item.title}</div>
+								<p className="max-w-[350px] leading-normal">{item.description}</p>
 							</div>
 							{/* <====<<==== Arrow ====>>====> */}
 							<div className="text-3xl">
-								<RxArrowTopRight />
+								<RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
 							</div>
 						</div>
 					</SwiperSlide>
--- pages/services/index.js
+++ pages/services/index.js
@@ -17,7 +17,9 @@ const Services = () => {
 						<p className="mb-4 max-w-[400px] mx-auto lg:mx-0">Lorem ipsum dolor sit, amet consectetor adipisicing elit. A eveniet dignissimos distinctio tempore harmore landiuen.</p>
 					</div>
 					{/* <====<<==== Slider ====>>====> */}
+					<div className="w-full xl:max-w-[65%]">
 					<ServiceSlider />
+					</div>
 				</div>
 			</div>
 			<Bulb />
