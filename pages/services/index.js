f713f66367e4d69f586d1df148801e6c0d572877
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-14 12:45:15 +0600

    Framer Motion Implemented On Service Page

--- pages/services/index.js
+++ pages/services/index.js
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import Circles from "./../../components/Circles";
 import Bulb from "./../../components/Bulb";
 import ServiceSlider from "../../components/ServiceSlider";
+import { fadeIn } from "./../../variants";
 
 const Services = () => {
 	return (
@@ -11,15 +12,37 @@ const Services = () => {
 				<div className="flex flex-col xl:flex-row gap-x-8">
 					{/* <====<<==== Text ====>>====> */}
 					<div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-						<h2 className="h2 xl:mt-8">
+						<motion.h2
+							variants={fadeIn("up", 0.2)}
+							initial="hidden"
+							animate="show"
+							exit="hidden"
+							className="h2 xl:mt-8"
+						>
 							My Services<span className="text-accent">.</span>
-						</h2>
-						<p className="mb-4 max-w-[400px] mx-auto lg:mx-0">Lorem ipsum dolor sit, amet consectetor adipisicing elit. A eveniet dignissimos distinctio tempore harmore landiuen.</p>
+						</motion.h2>
+						<motion.p
+							variants={fadeIn("up", 0.4)}
+							initial="hidden"
+							animate="show"
+							exit="hidden"
+							className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+						>
+							Lorem ipsum dolor sit, amet consectetor adipisicing
+							elit. A eveniet dignissimos distinctio tempore
+							harmore landiuen.
+						</motion.p>
 					</div>
 					{/* <====<<==== Slider ====>>====> */}
-					<div className="w-full xl:max-w-[65%]">
-					<ServiceSlider />
-					</div>
+					<motion.div
+						variants={fadeIn("up", 0.3)}
+						initial="hidden"
+						animate="show"
+						exit="hidden"
+						className="w-full xl:max-w-[65%]"
+					>
+						<ServiceSlider />
+					</motion.div>
 				</div>
 			</div>
 			<Bulb />
