220a14cde049041dc84cba6f0f6058f96b330d3a
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-15 05:57:00 +0600

    Modify The Motion Effect

--- components/Transition.js
+++ components/Transition.js
@@ -25,7 +25,7 @@ const Transition = () => {
 				initial="initial"
 				animate="animate"
 				exit="exit"
-				transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+				transition={{ delay: 0.2, duration: 0.2, ease: "easeInOut" }}
 			></motion.div>
 			<motion.div
 				className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3B2D71]"
@@ -33,7 +33,7 @@ const Transition = () => {
 				initial="initial"
 				animate="animate"
 				exit="exit"
-				transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+				transition={{ delay: 0.3, duration: 0.3, ease: "easeInOut" }}
 			></motion.div>
 			<motion.div
 				className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4B3792]"
@@ -41,7 +41,7 @@ const Transition = () => {
 				initial="initial"
 				animate="animate"
 				exit="exit"
-				transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+				transition={{ delay: 0.4, duration: 0.4, ease: "easeInOut" }}
 			></motion.div>
 		</>
 	);
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -13,8 +13,18 @@ const Contact = () => {
 					<form className="flex-1 flex flex-col gap-6 w-full mx-auto">
 						{/* <===<<=== Input Group ===>>===> */}
 						<div className="flex gap-x-6 w-full">
-							<input type="text" placeholder="name" className="input" />
-							<input type="text" placeholder="email" className="input" />
+							<input
+								type="text"
+								name="name"
+								placeholder="name"
+								className="input"
+							/>
+							<input
+								type="email"
+								name="email"
+								placeholder="email"
+								className="input"
+							/>
 						</div>
 						<input type="text" placeholder="Subject" className="input" />
 						<textarea
@@ -22,7 +32,9 @@ const Contact = () => {
 							className="textarea"
 						></textarea>
 						<button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
-							<span className="group-hover:-translate-y-[120px] group-hover:opacity-0 transition-all duration-500">Let&apos;s Talk</span>
+							<span className="group-hover:-translate-y-[120px] group-hover:opacity-0 transition-all duration-500">
+								Let&apos;s Talk
+							</span>
 							<BsArrowRight className="translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px] text-accent" />
 						</button>
 					</form>
